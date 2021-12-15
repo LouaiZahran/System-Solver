a969d968738c1fec895239a3a28e672b85f676c7
LouaiZahran <louai0nasr@gmail.com>
2021-12-15 23:39:26 +0200

    Added Forward and Backward Substitution Methods

--- linear/src/test/java/com/numerical/solver/linear/LinearApplicationTests.java
+++ linear/src/test/java/com/numerical/solver/linear/LinearApplicationTests.java
@@ -15,29 +15,25 @@ class LinearApplicationTests {
 	void contextLoads() {
 		Matrix matrix = new Matrix();
 		ArrayList<BigDecimal> row = new ArrayList<>();
-		row.add(new BigDecimal(2));
-		row.add(new BigDecimal(1));
+
+		matrix = new Matrix();
+		row.add(new BigDecimal("1"));
+		row.add(new BigDecimal("2"));
 		matrix.insertRow(row);
 		row.clear();
 
-		row.add(new BigDecimal(3));
-		row.add(new BigDecimal(4));
+		row.add(BigDecimal.ZERO);
+		row.add(new BigDecimal("2000000000"));
 		matrix.insertRow(row);
 		row.clear();
 
 		Matrix b = new Matrix();
-		row.add(new BigDecimal(5));
-		row.add(new BigDecimal(6));
+		row.add(new BigDecimal(2));
+		row.add(new BigDecimal(1));
 		b.insertColumn(row);
 
-		row.clear();
-		row.add(BigDecimal.ZERO);
-		row.add(BigDecimal.ZERO);
-		Matrix guess = new Matrix();
-		guess.insertColumn(row);
-
 		Solver solver = new Solver(matrix, b);
-		solver.solveIterative(guess, 100, 0.01, true);
+		solver.backwardSub().print();
 	}
 
 }
