cfdc62773d3990f976f5888ad72abc6f8a73d5b4
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2021-12-16 00:42:49 +0200

    Decomposer class added
    cholskey decomposition
    crout decomposition

---
+++ linear/src/main/java/com/numerical/solver/linear/model/Decomposer.java
@@ -0,0 +1,106 @@
+package com.numerical.solver.linear.model;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayList;
+
+public class Decomposer {
+    private Matrix coeff;
+    private Matrix constant;
+    private Matrix result;
+
+    public Decomposer(Matrix coeff, Matrix constant) throws IllegalArgumentException{
+        if(coeff.getDimension().getRow() != constant.getDimension().getRow()
+                || constant.getDimension().getCol() != 1)
+            throw new IllegalArgumentException("System's dimensions are not correct");
+        this.setCoeff(coeff);
+        this.setConstant(constant);
+    }
+
+    public Matrix getCoeff() {
+        return coeff;
+    }
+
+    public void setCoeff(Matrix coeff) {
+        this.coeff = new Matrix(coeff);
+    }
+
+    public Matrix getConstant() {
+        return constant;
+    }
+
+    public void setConstant(Matrix constant) {
+        this.constant = new Matrix(constant);
+    }
+
+    public Matrix getResult() {
+        return result;
+    }
+
+    public void setResult(Matrix result) {
+        this.result = result;
+    }
+
+    public Matrix cholskeyDecomposition(Matrix matrix)
+    {
+        int n=matrix.getDimension().getRow();
+        MathContext mc = new MathContext(10);
+        Matrix lower= new Matrix(constant.getDimension());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                BigDecimal sum = new BigDecimal(0);
+                if (j == i) {
+                    for (int k = 0; k < j; k++)
+                        sum.add(lower.getCell(new Dimension(j,k)).multiply(lower.getCell(new Dimension(j,k))));
+
+                    lower.setCell(new Dimension(j,j),
+                            matrix.getCell(new Dimension(j,j)).subtract(sum).sqrt(mc));
+                }
+                else {
+                    for (int k = 0; k < j; k++)
+                        sum.add(lower.getCell(new Dimension(i,k)).multiply(lower.getCell(new Dimension(j,k))));
+
+                    lower.setCell(new Dimension(i,j),
+                            matrix.getCell(new Dimension(i,j)).subtract(sum).divide(lower.getCell(new Dimension(j,j))));
+                }
+            }
+        }
+        //to be transposed and returned
+        return lower;
+    }
+    public ArrayList<Matrix> croutDecomposition(Matrix matrix) {
+        int n=matrix.getDimension().getRow();
+        Matrix lower = new Matrix(constant.getDimension());
+        Matrix upper = new Matrix(constant.getDimension());
+
+        for (int i = 0; i < n; i++) {
+            upper.setCell(new Dimension(i,i),new BigDecimal(1));
+        }
+
+        for (int j = 0; j < n; j++) {
+            for (int i = j; i < n; i++) {
+                BigDecimal sum = new BigDecimal(0);
+                for (int k = 0; k < j; k++)
+                    sum.add(lower.getCell(new Dimension(i,k)).multiply(upper.getCell(new Dimension(k,j))));
+
+                lower.setCell(new Dimension(i,j),matrix.getCell(new Dimension(i,j)).subtract(sum));
+            }
+
+            for (int i = j; i < n; i++) {
+                BigDecimal sum = new BigDecimal(0);
+                for(int k = 0; k < j; k++)
+                    sum.add(lower.getCell(new Dimension(j,k)).multiply(upper.getCell(new Dimension(k,i))));
+
+                if (lower.getCell(new Dimension(j,j)).equals(new BigDecimal(0)))
+                    System.out.printf("det(L) close to 0!\n Can't divide by 0...\n");
+
+                upper.setCell(new Dimension(j,i),
+                        matrix.getCell(new Dimension(j,i)).subtract(sum).divide(lower.getCell(new Dimension(j,j))));
+            }
+        }
+        ArrayList<Matrix> ret=new ArrayList<Matrix>();
+        ret.add(lower);
+        ret.add(upper);
+        return ret;
+    }
+}
