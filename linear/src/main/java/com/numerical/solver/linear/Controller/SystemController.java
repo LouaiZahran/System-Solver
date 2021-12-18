518212fe3661a736595fbc4c195d8450390b5ebd
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2021-12-19 01:46:33 +0200

    Backend

--- linear/src/main/java/com/numerical/solver/linear/Controller/SystemController.java
+++ linear/src/main/java/com/numerical/solver/linear/Controller/SystemController.java
@@ -1,5 +1,6 @@
 package com.numerical.solver.linear.Controller;
 
+import com.google.gson.Gson;
 import com.numerical.solver.linear.model.ApiProblem;
 import com.numerical.solver.linear.model.Decomposer;
 import com.numerical.solver.linear.model.Matrix;
@@ -21,29 +22,40 @@ import java.util.ArrayList;
 public class SystemController {
 
     ArrayList<ArrayList<BigDecimal>> generateData(BigDecimal[][] arrayData,int precision){
-        ArrayList<ArrayList<BigDecimal>> ret=new ArrayList<ArrayList<BigDecimal>>();
+        ArrayList<ArrayList<BigDecimal>> ret=new ArrayList<>();
 
         for(int i=0;i<arrayData.length;i++){
-            ret.add(new ArrayList<>());
+            ArrayList<BigDecimal> retRow=new ArrayList<>();
             for(int j=0;j<arrayData.length;j++){
-                ret.get(0).add(arrayData[i][j].round(new MathContext(precision, RoundingMode.HALF_UP)));
+                retRow.add(arrayData[i][j].round(new MathContext(precision, RoundingMode.HALF_UP)));
             }
+            ret.add(retRow);
         }
         return ret;
+
     }
     @PostMapping("/postProblem")
-    public String matrixGenerator(@RequestBody ApiProblem apiProblem){
-        /*
-        ApiProblem problem=new ApiProblem(apiProblem.getNumberofUnknown(),apiProblem.getCoeff_matrix(),
-                apiProblem.getConstant_matrix(),apiProblem.getPrecision(),apiProblem.getMethod());
-        System.out.println("hello");
+    public ArrayList<ArrayList<ArrayList<BigDecimal>>> matrixGenerator(@RequestBody String apiProblemString){
+        ApiProblem apiProblem=new Gson().fromJson(apiProblemString,ApiProblem.class);
         Decomposer decomposer=new Decomposer();
         Matrix coeffMatrix =new Matrix();
-        coeffMatrix.setData(generateData(problem.getCoeff_matrix(),problem.getPrecision()));
-        if(problem.getMethod()=="Crout Decompostion"){
-            return decomposer.croutDecomposition(coeffMatrix);
-        }*/
+        coeffMatrix.setData(generateData(apiProblem.getCoeff_matrix(),apiProblem.getPrecision()));
+        ArrayList<ArrayList<ArrayList<BigDecimal>>> result=new ArrayList<ArrayList<ArrayList<BigDecimal>>>();
+        if(apiProblem.getMethod().equalsIgnoreCase("Cholesky Decompostion")){
+            /*ArrayList<Matrix>decomposed = decomposer.cholskeyDecomposition(coeffMatrix);
+            result.add(decomposed.get(0).getData());
+            result.add(decomposed.get(1).getData());
+            for(int i=0;i<coeffMatrix.getData().size();i++){
+                for (int j=0;j<coeffMatrix.getData().size();j++){
+                    System.out.println(coeffMatrix.getData().get(i).get(j));
+                }
+            }*/
+            result.add(coeffMatrix.getData());
+            return result;
+        }
+        else if(apiProblem.getMethod().equalsIgnoreCase("Crout Decompostion")){
 
-        return apiProblem.getMethod();
+        }
+        return null;
     }
 }
