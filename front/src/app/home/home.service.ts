869e42e938067a3fc92fb1c5a1a3117e0e5e9f63
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2021-12-19 18:55:26 +0200

    Crout & cholesky Decomposition completed

--- front/src/app/home/home.service.ts
+++ front/src/app/home/home.service.ts
@@ -10,5 +10,5 @@ export class HomeService {
   constructor(private http: HttpClient) { }
   postProblem(problem : properties) : Observable<any>{
     return this.http.post<any>("http://localhost:8080/postProblem", problem);
-}
+  }
 }
