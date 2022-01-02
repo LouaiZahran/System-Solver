c9b9674cd608bcf79f1da5005b3087d1a6b5ac4e
Deffo0 <73228199+Deffo0@users.noreply.github.com>
2022-01-02 12:46:28 +0200

    home page

---
+++ front/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { I18nPluralPipe } from '@angular/common';
+import { variable } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { from } from 'rxjs';
+
+//-------------------------------------------------------------------------------//
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+  })
+export class homecomponent {
+
+
+  constructor(){}
+//-------------------------------------------------------------------------------//
+
+
+
+}
